77b8a3283a3fe10ab362c968b3bbb05728d994d8
tomas <tomynahuel23@gmail.com>
2026-07-21 17:01:30 -0300

    Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Cambia TU_USUARIO por tu nombre de usuario en GitHub
-  site: 'https://tomynahuel.github.io',
-  // Cambia TU_REPOSITORIO por el nombre que le darás a tu repositorio en GitHub
+  site: 'https://Tomynahuel.github.io',
   base: '/mi-timer',
 });
